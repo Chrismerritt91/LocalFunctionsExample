78f8455806ae68faa5b2d2da2b69548e089dbc59
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-04-03 11:07:12 -0500

    complete local functions lesson

--- Program.cs
+++ Program.cs
@@ -1,3 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Student {
 
+    public void DisplayMarks(params int[] marks) {
+
+        for(int i = 0; i < marks.Length; i++) {
+            Console.WriteLine("Marks " + (i+1) + ": " + marks[i]);
+
+        }
+
+        //assign value from local function to variable
+        double avgMarks = getAverageMarks();
+
+        Console.WriteLine("Average Marks: " + avgMarks);
+
+        //create local function to get average of students marks
+        double getAverageMarks() {
+            double sum = 0;
+            double avg;
+
+            for(int i=0; i<marks.Length; i++) {
+                sum += marks[i];
+            }
+
+            avg = (double) sum / marks.Length;
+
+            return avg;
+        }
+
+    }
+
+    class Program {
+
+        static void Main() {
+
+            //create an object
+            Student s = new Student();
+
+            //call method to list grades and display average
+            s.DisplayMarks(80,45,71);
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
